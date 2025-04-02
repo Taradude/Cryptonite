37ed77555df4dddc395fd1aa7e4e49c6b9626f9a
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-04-02 20:49:13 +0300

    add utm_data

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,61 @@
 import { onMounted } from 'vue'
 
 onMounted(() => {
-  document.title = 'Мінікурс Без Зусиль | Cryptonite'
+  document.title = 'Мінікурс Без Зусиль | Cryptonite';
+
+  function getUTMParams() {
+    const utm = {};
+    const query = window.location.search.substring(1);
+    const vars = query.split('&');
+    for (let i = 0; i < vars.length; i++) {
+      const pair = vars[i].split('=');
+      if (pair[0].indexOf('utm_') === 0) {
+        utm[pair[0]] = decodeURIComponent(pair[1]);
+      }
+    }
+    return utm;
+  };
+
+  function saveUTMs() {
+    const utmParams = getUTMParams();
+    if (Object.keys(utmParams).length) {
+      localStorage.setItem('utm_data', JSON.stringify(utmParams.utm_source));
+    } else {
+      localStorage.setItem('utm_data', "Нема UTM");
+    }
+  };
+
+  function initBtn() {
+    const btns = document.querySelectorAll('a[href="https://secure.wayforpay.com/button/ba17bd76f2043"]');
+    btns?.forEach((btn) => {
+      if (btn) {
+        btn.addEventListener('click', function(e) {
+          e.preventDefault();
+          const baseUrl = this.getAttribute('href');
+          
+          const form = document.createElement('form');
+          form.method = 'POST';
+          form.action = baseUrl;
+          form.style.display = 'none';
+          
+          const inputMerchantData = document.createElement('input');
+          inputMerchantData.type = 'hidden';
+          inputMerchantData.name = 'merchantData';
+          inputMerchantData.value = localStorage.getItem('utm_data') ?? "Нема UTM";
+          form.appendChild(inputMerchantData);
+          
+          document.body.appendChild(form);
+          form.submit();
+        });
+      }
+    })
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    saveUTMs();
+    initBtn();
+  });
+
 })
 </script>
 
@@ -37,4 +91,4 @@ html {
 body {
   background-color: $bg-color;
 }
-</style>
+</style>;
